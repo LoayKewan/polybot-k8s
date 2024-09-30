9448480737b53777c0b0367a81208737ac24fe5a
LoayKewan <loaykewan4@gmail.com>
2024-09-30 19:59:51 +0300

    new fix of os.environ

--- bot.py
+++ bot.py
@@ -162,13 +162,8 @@ class ObjectDetectionBot(Bot):
 
 
 
-
-
-
         elif "text" in msg and msg["text"] == "hi":
-            self.send_text(msg['chat']['id'],
-                           f"Hi: {msg['chat']['first_name']} {msg['chat']['last_name']}, how can I help you .....?")
-
+            self.send_text(msg['chat']['id'],f"Hi: {msg['chat']['first_name']} {msg['chat']['last_name']}, how can I help you .....?")
 
 
         elif "text" in msg and msg["text"] != "hi":
